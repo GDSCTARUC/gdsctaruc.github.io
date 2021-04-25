3c34ff9b260b18ab9dfd6cdebd0cb28e61f1e810
Chuah Seong Rong <chuahsr99@gmail.com>
2021-04-25 13:19:31 +0800

    update countdown

--- script/event.js
+++ script/event.js
@@ -4,30 +4,18 @@
         hour = minute * 60,
         day = hour * 24;
 
-    let birthday = "Sep 30, 2021 00:00:00",
-        countDown = new Date(birthday).getTime(),
+    let next_event_date = "Sep 30, 2021 00:00:00",
+        countDown = new Date(next_event_date).getTime(),
         x = setInterval(function() {
 
             let now = new Date().getTime(),
                 distance = countDown - now;
 
-            document.getElementById("days").innerText = Math.floor(distance / (day)),
-                document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-                document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-                document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+            document.getElementById("days").innerText = Math.floor(distance / (day));
+            document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour));
+            document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute));
+            document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
 
-            //do something later when date is reached
-            if (distance < 0) {
-                let headline = document.getElementById("headline"),
-                    countdown = document.getElementById("countdown"),
-                    content = document.getElementById("content");
-
-                headline.innerText = "It's my birthday!";
-                countdown.style.display = "none";
-                content.style.display = "block";
-
-                clearInterval(x);
-            }
             //seconds
         }, 0)
 }());
